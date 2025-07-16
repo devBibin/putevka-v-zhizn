17eb0af20518aa9cb06bf7cda883cb52d99429e2
dmitryLem <dlemesonok0@gmail.com>
2025-07-16 22:55:05 +0700

    feat: generate unique filename for files

--- documents/models.py
+++ documents/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
+import uuid
+
+def upload_to_path(instance, filename):
+    ext = filename.split('.')[-1]
+    new_filename = f'{filename.split(".")[0]}-{uuid.uuid4().hex}.{ext}'
+    return os.path.join('documents', instance.user.username, new_filename)
 
 class Document(models.Model):
     DOCUMENT_TYPE_CHOICES = [
@@ -10,7 +17,7 @@ class Document(models.Model):
     ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='documents')
-    file = models.FileField(upload_to='documents/')
+    file = models.FileField(upload_to=upload_to_path)
     caption = models.CharField(max_length=255, blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
