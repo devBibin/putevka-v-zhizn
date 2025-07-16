ffa81cd27a3067927a8784d6e1cdfd2448696e3a
dmitryLem <dlemesonok0@gmail.com>
2025-07-16 22:13:29 +0700

    fix: viewing of user's document, add document type

--- documents/admin.py
+++ documents/admin.py
@@ -20,8 +20,8 @@ class DocumentAdmin(admin.ModelAdmin):
 class DocumentInline(admin.TabularInline):
     model = Document
     extra = 0
-    fields = ('caption', 'file', 'uploaded_at')
-    readonly_fields = ('uploaded_at',)
+    fields = ('caption', 'document_type', 'file', 'uploaded_at')
+    readonly_fields = ('uploaded_at', 'caption', 'document_type', 'file')
 
     def get_formset(self, request, obj=None, **kwargs):
         formset = super().get_formset(request, obj, **kwargs)
