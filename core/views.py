f35327c4ff3f5584c83a6460887e4b83f12a6dee
dmitryLem <dlemesonok0@gmail.com>
2025-07-19 18:37:18 +0700

    feat: add viewing of notification

--- core/views.py
+++ core/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.views.decorators.http import require_POST
+
 from .bot import webhook
+from django.contrib.auth.decorators import login_required
+
+from .models import Notification
 
 
 def index(request):
@@ -40,3 +45,19 @@ def login_view(request):
             return redirect('index')
 
     return redirect('index')
+
+@login_required
+def notification_list(request):
+    notifications = Notification.objects.filter(recipient=request.user).order_by('is_seen', '-created_at')
+
+    return render(request, 'notifications/notifications_list.html', {'notifications': notifications})
+
+@login_required
+@require_POST
+def mark_notification_as_seen(request, notification_id):
+    notification = get_object_or_404(Notification, pk=notification_id, recipient=request.user)
+    notification.is_seen = True
+
+    notification.save()
+
+    return redirect('notifications')
