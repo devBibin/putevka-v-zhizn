1251f7f84566a13a066065583497bfa6d0454839
dmitryLem <dlemesonok0@gmail.com>
2025-07-22 18:53:45 +0700

    refactor: variable in config.py

--- config.py
+++ config.py
@@ -1,17 +1,25 @@
 import logging
 import os
+import json
 
 logger = logging.getLogger(__name__)
 
 try:
     CHAT_ID = os.getenv('CHAT_ID')
     logger.info(f'Сообщения в тг отправляются локальному пользователю {CHAT_ID}')
-except:
+except Exception as e:
     CHAT_ID = None
-    logger.error('CHAT_ID для дебага не найден')
+    logger.error(f'CHAT_ID для дебага не найден {e}')
 
-TELEGRAM_STAFF_CHAT_IDS = {
-    'developer': CHAT_ID
-}
+try:
+    raw_dict = os.getenv('TELEGRAM_STAFF_CHAT_IDS')
+    TELEGRAM_STAFF_CHAT_IDS = json.loads(raw_dict)
+except Exception as e:
+    TELEGRAM_STAFF_CHAT_IDS = None
+    logger.error(f'TELEGRAM_STAFF_CHAT_IDS не используется {e}')
 
-TELEGRAM_LOG_CHAT_ID = CHAT_ID
+try:
+    TELEGRAM_LOG_CHAT_ID = os.getenv('TELEGRAM_LOG_CHAT_ID')
+except Exception as e:
+    TELEGRAM_LOG_CHAT_ID = None
+    logger.error(f'TELEGRAM_LOG_CHAT_ID не используется {e}')
