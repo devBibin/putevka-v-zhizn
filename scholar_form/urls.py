dfb26d21ded19f8089ee3d3de61ceff845bb4b97
dmitryLem <dlemesonok0@gmail.com>
2025-11-09 22:24:52 +0700

    add new page with interview preparation

--- scholar_form/urls.py
+++ scholar_form/urls.py
@@ -2,16 +2,18 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import path
 
+import review_by_tutor
+from core.decorators import ensure_registration_gate
 from review_by_tutor.views import testing_list_for_candidate
 from scholar_form import views
-from core.decorators import ensure_registration_gate
 from .forms import ApplicationWizard, FORMS
 
 urlpatterns = [
-    path('apply/', ensure_registration_gate('protected')(login_required(ApplicationWizard.as_view(FORMS))), name='apply'),
+    path('apply/', ensure_registration_gate('protected')(login_required(ApplicationWizard.as_view(FORMS))),
+         name='apply'),
     path('thank-you/', lambda request: render(request, 'thank_you.html'), name='thank_you'),
     path('personal-info/', views.personal_info, name='personal_info'),
     path("video/", views.my_video_page, name="my_video_page"),
     path("testing/", testing_list_for_candidate, name="candidate_testing_list"),
-
+    path("preparation/", review_by_tutor.views.interview_preparation_view, name="preparation"),
 ]
