a249ebfd98d4829a9e4e8a98e875ae3cca87f015
dmitryLem <dlemesonok0@gmail.com>
2026-01-30 23:47:17 +0700

    подключил почтовый клиент яндекс клауда

--- Putevka/settings.py
+++ Putevka/settings.py
@@ -149,15 +149,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DEFAULT_FROM_EMAIL = 'no-reply@yourdomain.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'postbox.cloud.yandex.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_email_password_or_app_password'
+
+EMAIL_HOST_USER = os.getenv("YC_EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.getenv("YC_EMAIL_PASSWORD")
+
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
